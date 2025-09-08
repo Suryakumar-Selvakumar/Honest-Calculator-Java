d3141521cf3bd661dedc0aaccf1fb216491e19a4
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2025-09-08 10:33:19 -0600

    feat: Complete task 2

--- Honest Calculator (Java)/task/src/honestcalculator/Main.java
+++ Honest Calculator (Java)/task/src/honestcalculator/Main.java
@@ -11,23 +11,42 @@ public class Main {
         while (true) {
             System.out.println("Enter an equation");
             String[] equation = sc.nextLine().trim().split(" ");
-            boolean num1 = equation[0].matches(numRegex);
-            boolean num2 = equation[2].matches(numRegex);
+            boolean isXValid = equation[0].matches(numRegex);
+            boolean isYValid = equation[2].matches(numRegex);
 
-            if (!num1 || !num2) {
+            if (!isXValid || !isYValid) {
                 System.out.println("Do you even know what numbers are? Stay focused!");
                 continue;
             }
 
             String operator = equation[1];
-            if (operator.equals("+") ||
-                    operator.equals("-") ||
-                    operator.equals("*") ||
-                    operator.equals("/")) {
-                break;
-            } else {
+            if (!operator.equals("+") &&
+                    !operator.equals("-") &&
+                    !operator.equals("*") &&
+                    !operator.equals("/")) {
                 System.out.println("Yes ... an interesting math operation. You've slept through all classes, haven't you?");
+                continue;
             }
+
+
+            float x = Float.parseFloat(equation[0]);
+            float y = Float.parseFloat(equation[2]);
+
+            if (operator.equals("/") && y == 0) {
+                System.out.println("Yeah... division by zero. Smart move...");
+                continue;
+            }
+
+            float result = switch (operator) {
+                case "+" -> x + y;
+                case "-" -> x - y;
+                case "*" -> x * y;
+                case "/" -> x / y;
+                default -> 0;
+            };
+
+            System.out.println(result);
+            break;
         }
     }
 }
